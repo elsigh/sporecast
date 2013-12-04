770cb73bdb4acd611719432d2f1c072268b2a19f
Lindsey Simon <elsigh@gmail.com>
2013-12-03 18:12:26 -0800

    adding update script

---
+++ server/wunderground/update.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/sh
+
+python history.py;
+python forecast.py;
+python format.py data/;
+git add data/*;
+git commit -a -m 'Adding new wunderground data.';
+git push;
+cd ../;
+./update.sh;
+echo 'YEAH!! Done.\n'
